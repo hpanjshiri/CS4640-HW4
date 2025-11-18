2092a5834c85953c1461fdbd778dc3bb5180c9a6
michelleegao <163981352+michelleegao@users.noreply.github.com>
2025-11-18 14:36:57 -0500

    changed file location

--- js/game.js
+++ js/game.js
@@ -201,7 +201,8 @@ document.getElementById("guessForm").addEventListener("submit", async (e) => {
     
     renderGame();
 
-    if (guess === state.currentGame.target) {
-        endGame();
-    }
+    // if (guess === state.currentGame.target) {
+    //     endGame();
+    // }
 });
+
